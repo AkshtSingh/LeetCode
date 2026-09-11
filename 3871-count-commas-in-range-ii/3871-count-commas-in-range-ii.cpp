fde149d967340003d17ab88076794340960c5fe8
AkshatSingh <akshat22411@gmail.com>
2026-09-11 21:25:01 +0530

    Time: 1 ms (7.9%), Space: 8.9 MB (82.91%) - LeetHub

---
+++ 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    long long countCommas(long long n) {
+        long long ans = 0;
+        long long threshold = 1000;
+
+        while (n >= threshold) {
+            ans += (n - threshold + 1);
+            if (threshold > LLONG_MAX / 1000) break; // prevent 64-bit overflow
+            threshold *= 1000;
+        }
+
+        return ans;
+    }
+};
